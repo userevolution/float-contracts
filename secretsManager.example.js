33846b1a02352236d20a8353ec669b148e5c157a
denhampreen <denhampreen@gmail.com>
2021-02-03 10:58:26 +0200

    secrets manager and explicit version for postgres

---
+++ secretsManager.example.js
@@ -0,0 +1,24 @@
+const mnemonic =
+  ""; 
+const mainnetProviderUrl =
+  "";
+const rinkebyProviderUrl =
+  "";
+const goerliProviderUrl =
+  "";
+const kovanProviderUrl =
+  "";
+const binanceTest = "";
+​
+const etherscanApiKey = "";
+const bscscanApiKey = "";
+module.exports = {
+  mnemonic,
+  mainnetProviderUrl,
+  rinkebyProviderUrl,
+  goerliProviderUrl,
+  kovanProviderUrl,
+  binanceTest,
+  etherscanApiKey,
+  bscscanApiKey,
+};
