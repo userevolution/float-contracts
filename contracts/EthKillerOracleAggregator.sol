b669737df48b6fd65d436723b8556e1058ed3902
jonjon <jonjonclark@gmail.com>
2021-02-19 11:46:03 +0200

    Basic oracle changes

--- contracts/EthKillerOracleAggregator.sol
+++ contracts/EthKillerOracleAggregator.sol
@@ -9,115 +9,63 @@ import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 contract EthKillerOracleAggregator is Initializable {
     using SafeMathUpgradeable for uint256;
 
-    uint256 public trxPrice;
-    uint256 public eosPrice;
-    uint256 public xrpPrice;
-    uint256 public indexPrice;
+    address public admin; // This will likely be the Gnosis safe
 
-    function setup(
-        uint256 _trxPrice,
-        uint256 _eosPrice,
-        uint256 _xrpPrice
-    ) public initializer {
-        trxPrice = _trxPrice;
-        eosPrice = _eosPrice;
-        xrpPrice = _xrpPrice;
+    int256 public indexPrice;
+
+    int256 public tronPrice;
+    int256 public eosPrice;
+    int256 public xrpPrice;
+
+    function setup(address _admin) public initializer {
+        admin = _admin;
+
+        // Initialising asset prices
+        tronPrice = _getAssetPrice(0);
+        eosPrice = _getAssetPrice(1);
+        xrpPrice = _getAssetPrice(2);
+
+        // Initialiasing base index price
         indexPrice = 1e18;
     }
 
-    function _getAssetPrice(uint256 index) internal returns (uint256) {
-        // Band get Price of index
-        return 10;
-    }
-
-    function _calcAbsolutePercentageChange(
-        uint256 newPrice,
-        uint256 basePrice,
-        bool isPositiveMovement
-    ) internal returns (uint256) {
-        if (isPositiveMovement) {
-            return (newPrice.sub(basePrice)).mul(1e18).div(basePrice);
-        } else {
-            return (basePrice.sub(newPrice)).mul(1e18).div(basePrice);
-        }
+    function _getAssetPrice(uint256 index) internal returns (int256) {
+        // index 0 = tron
+        // index 1 =  eos
+        // index 2 = ripple
+        // Call to band oracle for assets price
+        return 1e18;
     }
 
     function _calculatePrice() internal {
-        uint256 newTrxPrice = _getAssetPrice(0);
-        uint256 newEosPrice = _getAssetPrice(1);
-        uint256 newXrpPrice = _getAssetPrice(2);
+        int256 newTronPrice = _getAssetPrice(0);
+        int256 newEosPrice = _getAssetPrice(1);
+        int256 newXrpPrice = _getAssetPrice(2);
 
-        bool isPositiveTrxPercentageChange = newTrxPrice > trxPrice;
-        bool isPositiveEosPercentageChange = newEosPrice > eosPrice;
-        bool isPositiveXrpPercentageChange = newXrpPrice > xrpPrice;
+        int256 valueOfChangeInIndex =
+            (indexPrice *
+                (_calcAbsolutePercentageChange(newTronPrice, tronPrice) +
+                    _calcAbsolutePercentageChange(newEosPrice, eosPrice) +
+                    _calcAbsolutePercentageChange(newXrpPrice, xrpPrice))) /
+                (3 * 1e18);
 
-        uint256 trxPercentageChange =
-            _calcAbsolutePercentageChange(
-                newTrxPrice,
-                trxPrice,
-                isPositiveTrxPercentageChange
-            );
-        uint256 eosPercentageChange =
-            _calcAbsolutePercentageChange(
-                newEosPrice,
-                eosPrice,
-                isPositiveEosPercentageChange
-            );
-        uint256 xrpPercentageChange =
-            _calcAbsolutePercentageChange(
-                newXrpPrice,
-                xrpPrice,
-                isPositiveXrpPercentageChange
-            );
-
-        uint256 accumulatedPercentagePositive = 0;
-        uint256 accumulatedPercentageNegative = 0;
-
-        if (isPositiveTrxPercentageChange) {
-            accumulatedPercentagePositive.add(trxPercentageChange);
-        } else {
-            accumulatedPercentageNegative.add(trxPercentageChange);
-        }
-        if (isPositiveEosPercentageChange) {
-            accumulatedPercentagePositive.add(eosPercentageChange);
-        } else {
-            accumulatedPercentageNegative.add(eosPercentageChange);
-        }
-        if (isPositiveXrpPercentageChange) {
-            accumulatedPercentagePositive.add(xrpPercentageChange);
-        } else {
-            accumulatedPercentageNegative.add(xrpPercentageChange);
-        }
-
-        bool isPositiveChange =
-            accumulatedPercentagePositive > accumulatedPercentageNegative;
-
-        uint256 averageMovement = 0;
-
-        if (isPositiveChange) {
-            averageMovement = accumulatedPercentagePositive.sub(
-                accumulatedPercentageNegative
-            );
-        } else {
-            averageMovement = accumulatedPercentageNegative.sub(
-                accumulatedPercentagePositive
-            );
-        }
-
-        uint256 priceMovement = indexPrice.mul(averageMovement);
-
-        if (isPositiveChange) {
-            indexPrice = indexPrice.add(priceMovement);
-        } else {
-            indexPrice = indexPrice.sub(priceMovement);
-        }
-
-        trxPrice = newTrxPrice;
+        // Set new prices
+        tronPrice = newTronPrice;
         eosPrice = newEosPrice;
         xrpPrice = newXrpPrice;
+
+        // Set new index price
+        indexPrice = indexPrice + valueOfChangeInIndex;
     }
 
-    function getPrice() public returns (uint256) {
+    function _calcAbsolutePercentageChange(int256 newPrice, int256 basePrice)
+        internal
+        returns (int256)
+    {
+        return ((newPrice - basePrice) * (1e18)) / (basePrice);
+    }
+
+    function getLatestPrice() external returns (int256) {
         _calculatePrice();
         return indexPrice;
     }
